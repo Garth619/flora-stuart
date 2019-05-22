a696ca50b4230dee23c85d77ef253c32d498ac5a
Garrett Cullen <garrettwcullen@gmail.com>
2019-05-22 10:32:01 -0700

    push

--- wp-content/themes/florastuart/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/florastuart/page-templates/homepage_template_parts/section-1.php
@@ -1,11 +1,77 @@
 <section id="section_one">
 	
 	
-	
+	<div class="inner_sec_one">
+		
+		<span class="sec_one_header">The law firm that cares about you</span><!-- sec_one_header -->
+		
+		<a class="free_consulation" href="#consultation">
+			
+			<?php echo file_get_contents("wp-content/themes/florastuart/images/arrow.svg"); ?>
+			
+		</a><!-- free_consulation -->
+		
+		<span class="featured_on">Flora's Story Featured On</span><!-- featured_on -->
+		
+		<div class="featured_wrapper">
+			
+			<div class="featured_slider">
+				
+				<div class="featured_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/header_logo-01.svg"/>
+					
+				</div><!-- featured_slide -->
+				
+				<div class="featured_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/header_logo-02.svg"/>
+					
+				</div><!-- featured_slide -->
+				
+				<div class="featured_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/header_logo-03.svg"/>
+					
+				</div><!-- featured_slide -->
+				
+				<div class="featured_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/header_logo-04.svg"/>
+					
+				</div><!-- featured_slide -->
+				
+				<div class="featured_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/header_logo-05.svg"/>
+					
+				</div><!-- featured_slide -->
+				
+			</div><!-- featured_slider -->
+			
+		</div><!-- featured_wrapper -->
+		
+		<div class="play_wrapper">
+			
+			<a class="" href="">
+			
+				<div class="play_outer">
+				
+					<div class="play_inner">
+					
+					
+					
+					</div><!-- play_inner -->
+				
+				</div><!-- play_outer -->
+			
+			</a>
+			
+		</div><!-- play_wrapper -->
+		
+	</div><!-- inner_sec_one -->
 
 	
-	
-
 	<picture>
 		
 <!-- 		<source type="image/webp" media="(max-width: 1450px)" srcset="<?php echo $section_one_banner_ipad_webp['url']; ?>" > -->
