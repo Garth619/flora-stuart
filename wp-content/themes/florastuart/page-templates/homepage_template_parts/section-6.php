fe224bf255ec612a62abfac902ab2df982c45c4d
Garrett Cullen <Garth619@users.noreply.github.com>
2019-05-27 12:42:27 -0700

    push

---
+++ wp-content/themes/florastuart/page-templates/homepage_template_parts/section-6.php
@@ -0,0 +1,27 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		<span class="sec_six_title">We have recovered millions in settlements & verdicts</span><!-- sec_six_title -->
+		
+		<a class="sec_six_button" href="">View More Results</a><!-- sec_six_button -->
+		
+		<div class="sec_six_grid">
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+			<div class="sec_six_box"></div><!-- sec_six_box -->
+			
+		</div><!-- sec_six_grid -->
+		
+	</div><!-- sec_six_inner -->
+	
+</section><!-- section_six -->
