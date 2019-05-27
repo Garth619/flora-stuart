fc9efbf4f2ce5df05f1586a70564f99b93e16ac1
Garrett Cullen <Garth619@users.noreply.github.com>
2019-05-27 16:22:11 -0700

    push

--- wp-content/themes/florastuart/page-templates/homepage_template_parts/section-6.php
+++ wp-content/themes/florastuart/page-templates/homepage_template_parts/section-6.php
@@ -8,17 +8,82 @@
 		
 		<div class="sec_six_grid">
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<span class="case_title">$2.8 million</span><!-- case_title -->
+				
+				<span class="case_type">Brain Injury</span><!-- case_type -->
+				
+				<span class="case_description">A young man with a family who suffered permanent brain damage and a crushed skull.</span><!-- case_description -->
+				
+			</div><!-- sec_six_box -->
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/intl_results_img_01.jpg"/>
+				
+			</div><!-- sec_six_box -->
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<span class="case_title">$2.9 million</span><!-- case_title -->
+				
+				<span class="case_type">Practice Area</span><!-- case_type -->
+				
+				<span class="case_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</span><!-- case_description -->
+
+				
+			</div><!-- sec_six_box -->
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/intl_results_img_02.jpg"/>
+				
+			</div><!-- sec_six_box -->
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<span class="case_title">$1.7 million</span><!-- case_title -->
+				
+				<span class="case_type">Rear End Collision</span><!-- case_type -->
+				
+				<span class="case_description">A commercial truck rear-ended our clients with severe injuries.</span><!-- case_description -->
+
+				
+			</div><!-- sec_six_box -->
 			
-			<div class="sec_six_box"></div><!-- sec_six_box -->
+			<div class="sec_six_box">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/intl_results_img_03.jpg"/>
+				
+			</div><!-- sec_six_box -->
+			
+			<div class="sec_six_box">
+				
+				<span class="case_title">$1.45 million</span><!-- case_title -->
+				
+				<span class="case_type">Wrongful Death</span><!-- case_type -->
+				
+				<span class="case_description">A child was stuck on a road by a negligent driver.</span><!-- case_description -->
+
+			</div><!-- sec_six_box -->
+			
+			<div class="sec_six_box">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/intl_results_img_04.jpg"/>
+				
+			</div><!-- sec_six_box -->
+			
+			<div class="sec_six_box">
+				
+				<span class="case_title">$1.65 million</span><!-- case_title -->
+				
+				<span class="case_type">Semi Truck</span><!-- case_type -->
+				
+				<span class="case_description">A semi-truck ran a traffic light, hitting our client and causing fractured legs.</span><!-- case_description -->
+
+				
+			</div><!-- sec_six_box -->
 			
 		</div><!-- sec_six_grid -->
 		
