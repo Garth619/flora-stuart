187b706929f3575be0991ad5290abff211b48796
Garrett Cullen <Garth619@users.noreply.github.com>
2019-05-27 20:21:13 -0700

    push

--- wp-content/themes/florastuart/footer.php
+++ wp-content/themes/florastuart/footer.php
@@ -1,6 +1,80 @@
 <footer id="footer_trigger">
 	
-	<?php // gravity_form(1, false, false, false, '', true, 12); ?>
+	<div class="footer_inner">
+		
+		<div class="field_trip_wrapper">
+			
+			<div class="field_trip_inner">
+			
+			<img class="poppy_img" src="<?php bloginfo('template_directory');?>/images/footer_img_01.jpg"/>
+			
+			<span class="poppy_title">poppy’s field trip visits</span><!-- poppy_title -->
+			
+			<span class="comm_description">Flora Stuart is dedicated to making a difference in the lives of her clients. Our law firm is in the community to make a difference in the lives of others. </span><!-- comm_description -->
+			
+			<a class="visit_comm" href="">Visit Our Community Page</a><!-- visit_comm -->
+			
+			<div class="footer_play_wrapper">
+				
+				<a href="">
+				
+					<img class="footer_redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+				
+					<div class="play_image_wrapper">
+					
+						<img class="play_image" src="<?php bloginfo('template_directory');?>/images/footer_play.png"/>
+					
+					</div><!-- play_image_wrapper -->
+					
+					<span class="play_tri"></span><!-- play_tri -->
+				
+				</a>
+				
+			</div><!-- footer_play_wrapper -->
+			
+			</div><!-- field_trip_inner -->
+			
+		</div><!-- field_trip_wrapper -->
+		
+		<div class="footer_locations">
+			
+			<span class="footer_location_title">Phone</span><!-- footer_location_title -->
+			
+			<a class="footer_phone" href="tel:888782909">Call 24/7 (888) 782-9090</a>
+			
+			<span class="footer_location_title">Office Locations</span><!-- footer_location_title -->
+			
+			<div class="locations">
+				
+				<div class="single_location">
+					
+					<span class="address">213 S Green St<br/> Glasgow, KY 42141</span><!-- address -->
+					
+					<a class="get_directions" href="" target="_blank" rel="noopener">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- single_location -->
+				
+				<div class="single_location">
+					
+					<span class="address">607 E 10th Ave<br/> Bowling Green, KY 42101</span><!-- address -->
+					
+					<a class="get_directions" href="" target="_blank" rel="noopener">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- single_location -->
+				
+			</div><!-- locations -->
+			
+		</div><!-- footer_locations -->
+		
+		<div class="footer_form">
+			
+			<?php gravity_form(1, false, false, false, '', true, 12); ?>
+			
+		</div><!-- footer_form -->
+		
+	</div><!-- footer_inner -->
+	
+	
 		
 </footer>
 
