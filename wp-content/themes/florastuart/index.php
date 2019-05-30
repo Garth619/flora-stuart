fcb0d15c648b3a016813886fce53f4219292bc86
Garrett Cullen <garrettwcullen@gmail.com>
2019-05-30 11:32:20 -0700

    push

--- wp-content/themes/florastuart/index.php
+++ wp-content/themes/florastuart/index.php
@@ -1,16 +1,52 @@
 <?php get_header(); ?>
 
 
-
+<div id="internal_trigger" class="internal_main">
+	
+	<div class="internal_banner">
+		
+		<div class="internal_banner_content">
+			
+			<span class="banner_title">The law firm that cares about you</span><!-- banner_title -->
+			
+			<a class="internal_button" href="#consultation">
 				
-	<?php get_template_part( 'loop', 'index' );?>
+				<span>Request Free Evaluation</span>
 				
+				<img src="<?php bloginfo('template_directory');?>/images/arrow.svg"/>
 			
+			</a><!-- internal_button -->
 			
-	<?php // get_sidebar('blog'); ?>
+		</div><!-- internal_banner_content -->
+		
+		<img class="banner_bg" src="<?php bloginfo('template_directory');?>/images/intl_blog_header.jpg"/>
+		
+	</div><!-- internal_banner -->
+	
+	<div class="internal_wrapper">
+		
+		<div class="internal_container two_col">
 			
-
-
-
+			<div class="internal_content content">
+				
+				<h1 class="internal_header">All About Law Blog</h1><!-- internal_header -->
+				
+				<div class="blog_feed">
+				
+					<?php get_template_part( 'loop', 'index' ); ?>
+				
+				</div><!-- blog_feed -->
+				
+			</div><!-- internal_content -->
+			
+						
+			<?php get_sidebar('blog'); ?>
+			
+		</div><!-- internal_container -->
+		
+	</div><!-- internal_wrapper -->
+	
+</div><!-- internal_main -->
+		
 
 <?php get_footer(); ?>
--- wp-content/themes/florastuart/loop.php
+++ wp-content/themes/florastuart/loop.php
@@ -19,17 +19,29 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	
+	<div class="blog_post">
 		
-	<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+		<div class="blog_meta">
 		
-	<?php $pfx_date = get_the_date(); echo $pfx_date ?>
+			<span><?php $pfx_date = get_the_date(); echo $pfx_date ?></span>
 			
-	<?php echo get_the_category_list();?>
+			<?php echo get_the_category_list();?>
+		
+		</div><!-- blog_meta -->
+		
+		<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+		
+		<div class="blog_content">
+		
+			<?php echo wp_trim_words( get_the_content(), 54, '...' );?>
 			
-	<?php echo wp_trim_words( get_the_content(), 54, '...' );?>
-			
-	<?php edit_post_link( __( 'Edit'), '', '' ); ?>
-
+		</div><!-- blog_content -->
+		
+		<a class="read_more_blog" href="">Read More</a><!-- read_more_blog -->
+		
+		<?php edit_post_link( __( 'Edit'), '', '' ); ?>
+		
+</div><!-- blog_post -->
 			
 		
 <?php endwhile; // end of loop ?>
