d0c38f107a06b1d9c1a0c4562a87ae8ac5925792
Garrett Cullen <garrettwcullen@gmail.com>
2019-05-30 15:45:16 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-padirectory.php
+++ wp-content/themes/florastuart/page-templates/template-padirectory.php
@@ -5,8 +5,73 @@
 get_header(); ?>
 
 
+	<div id="internal_trigger" class="internal_main">
+	
+		<h1 class="internal_centered_header"><?php the_title();?></h1><!-- internal_centered_header -->
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		<div class="pa_directory">
+			
+			<div class="pa_directory_box">
+				
+				<div class="pa_img">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/intl_pa_img_01.jpg"/>
+				
+				</div><!-- pa_img -->
+				
+				<div class="pa_lower_content">
+					
+					<h2>Auto Accidents</h2>
+					
+					<ul>
+						<li><a href="">Car Accidents</a></li>
+						<li><a href="">Truck Accidents</a></li>
+						<li><a href="">Motorcycle Accidents</a></li>
+						<li><a href="">Drunk Drivers (DUI</a></li>
+						<li><a href="">Pedestrian</a></li>
+					</ul>
+					
+				</div><!-- pa_lower_content -->
+				
+			</div><!-- pa_directory_box -->
+			
+			<div class="pa_directory_box">
+				
+				<div class="pa_img">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/intl_pa_img_02.jpg"/>
+				
+				</div><!-- pa_img -->
+				
+				<div class="pa_lower_content">
+					
+					<h2>Auto Accidents</h2>
+					
+				</div><!-- pa_lower_content -->
+				
+			</div><!-- pa_directory_box -->
+			
+			<div class="pa_directory_box">
+				
+				<div class="pa_img">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/intl_pa_img_03.jpg"/>
+				
+				</div><!-- pa_img -->
+				
+				<div class="pa_lower_content">
+					
+					<h2>Auto Accidents</h2>
+					
+				</div><!-- pa_lower_content -->
+				
+			</div><!-- pa_directory_box -->
+						
+		</div><!-- pa_directory -->
+		
+	</div><!-- internal_main -->
+	
+		
 
 
 <?php get_footer(); ?>
