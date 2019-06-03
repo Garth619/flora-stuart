5e3327996dccc13b8c6c28467e1d47134aa4df7f
Garrett Cullen <garrettwcullen@gmail.com>
2019-06-03 15:23:31 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-paextended.php
+++ wp-content/themes/florastuart/page-templates/template-paextended.php
@@ -51,15 +51,21 @@
 							
 							<img class="red_img" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
 							
-							<img class="faq_profile_img" src="<?php bloginfo('template_directory');?>/images/sec_five_3.png"/>
+							<?php $testimonial_profile_image = get_field( 'testimonial_profile_image_extended' ); ?>
+							
+							<?php if ( $testimonial_profile_image ) { ?>
+							
+							<img class="faq_profile_img" src="<?php echo $testimonial_profile_image['url']; ?>" alt="<?php echo $testimonial_profile_image['alt']; ?>" />
+							
+							<?php } ?>
 							
 							<img class="faq_stars" src="<?php bloginfo('template_directory');?>/images/intl_header_icon_01.svg"/>
 							
-							<span class="faq_quote_title">"Our Son was in the accident with us.</span><!-- faq_quote_title -->
+							<span class="faq_quote_title"><?php the_field( 'testimonial_title_extended' ); ?></span><!-- faq_quote_title -->
 							
-							<span class="faq_quote_description">Flora took his case and was able to get him an annuity and to have it set up for later on to use it for college … We called several other lawyers and never got a response as fast as Flora did … They were there within an hour.”</span><!-- faq_quote_description -->
+							<span class="faq_quote_description"><?php the_field( 'testimonial_description_extended' ); ?></span><!-- faq_quote_description -->
 							
-							<span class="faq_profile_name">Josh R.</span><!-- faq_profile_name -->
+							<span class="faq_profile_name"><?php the_field( 'testimonial_name_extended' ); ?></span><!-- faq_profile_name -->
 							
 						</div><!-- faq_profile -->
 						
@@ -71,18 +77,27 @@
 			
 			<div class="faq_questions">
 				
-				<span class="faq_header">FAQs</span><!-- faq_header -->
+				<span class="faq_header"><?php the_field( 'faq_title' ); ?></span><!-- faq_header -->
 				
-				<ul>
-					<li>
-						<span class="question">How do I pay for an attorney in a semi accident injury case?</span><!-- question -->
-						<span class="answer">The Law Firm of Flora Templeton Stuart works on a contingency fee basis. That means we only get paid if you get paid in your truck wreck injury case. Call Warren and Barren County, Kentucky semi-truck accident attorney Flora Templeton Stuart at (888) 782-9090 for a free initial consultation twenty-four hours a day/seven days a week.</span><!-- answer -->
-					</li>
-					<li>
-						<span class="question">What compensation is available in a semi wreck injury case?</span><!-- question -->
-						<span class="answer">In trucking injury cases, multiple parties could be held liable for your injuries. An experienced truck wreck attorney can help you find all the negligent parties and seek full compensation for your injuries. In any commercial truck collision injury case, your recovery should take into account the immediate needs of you and your family as well as all future expenses including medical bills, rehabilitation or therapy, lost earning capacity, and long-term care, if needed. Victims may also be entitled to seek punitive damages, pain and suffering, and other compensation against the at-fault parties. In some cases, the victim’s family can seek restitution for loss of consortium (companionship) or a wrongful death claim if the injuries lead to the passing of their loved one. Contact us 24/7 if you are the victim of a semi-injury.</span><!-- answer -->
-					</li>
-				</ul>
+				
+				<?php if(get_field('faq_qa')): ?>
+				
+					<ul>
+				 
+					<?php while(has_sub_field('faq_qa')): ?>
+				 
+						<li>
+						
+							<span class="question"><?php the_sub_field( 'question' ); ?></span><!-- question -->
+							<span class="answer"><?php the_sub_field( 'answer' ); ?></span><!-- answer -->
+						
+						</li>
+				    
+					<?php endwhile; ?>
+					
+					</ul>
+				 
+				<?php endif; ?>
 				
 			</div><!-- faq_questions -->
 			
@@ -94,13 +109,7 @@
 		
 		<div class="extended_content_inner content">
 		
-			<h2>Call us 7 days a week, even on weekends.</h2>
-
-			<p>Faecenas id eros at enim maximus efficitur sit amet sed justo. Sed mollis fermentum cursus. Sed dapibus eu elit et blandit. Quisque in tellus vestibulum, cursus nulla ut, condimentum sapien. Donec malesuada turpis et eleifend lobortis. Donec purus odio, facilisis et eleifend vitae, congue sed nulla. Donec ante turpis, aliquam efficitur aliquam sit amet, bibendum tincidunt leo. Donec ultrices purus augue, eu pellentesque ante rutrum sit amet. Pellentesque ut augue nec enim euismod dapibus. Vivamus blandit mi velit, ornare consectetur enim gravida eget. Praesent odio purus, aliquet id malesuada at, vulputate iaculis enim. Curabitur sem nunc, tincidunt vitae varius ut, pellentesque et tellus. Duis vel sapien et lectus laoreet tincidunt. Vestibulum consectetur vulputate odio, at condimentum nisl lacinia at. In aliquam faucibus vehicula. Faecenas id eros at enim maximus efficitur sit amet sed justo. Sed mollis fermentum cursus. Sed dapibus eu elit et blandit. Quisque in tellus vestibulum, cursus nulla ut, condimentum sapien. Donec malesuada turpis et eleifend lobortis.</p>
-		
-			<p>Duis finibus neque vitae lectus auctor aliquet. Aliquam vel nisl iaculis. Proin at ipsum sed nibh vehicula tempor sit amet vitae orci. Nullam at dignissim urna, lobortis hendrerit dui. Sed ligula erat, sollicitudin at elit et, posuere placerat tortor. Etiam nibh mauris, pulvinar at diam sed, pellentesque ullamcorper ex. Quisque laoreet congue bibendum. Donec orci  mauris, efficitur eu enim ut, auctor volutpat enim.  Duis finibus neque vitae lectus auctor aliquet. Aliquam vel nisl iaculis. Proin at ipsum sed at diam sed, pellentesque ullamcorper ex. Quisque laoreet congue bibendum. Donec orci nibh vehicula tempor sit amet vitae orci. Nullam at dignissim urna, lobortis hendrerit dui. Sed ligula erat, sollicitudin at elit et, posuere placerat tortor.</p>
-
-			<p>unc dapibus commodo telluseget giat. Maecenas lectus ante, bibendum ut enim in, dapibus bibendum ligula. Fusce lacus nibh, ultrices a magna ac, sollicitudin pharetra turpis. Etiam finibus sed erat vitae iaculis. In varius molestie erat. Proin ligula nisi, mattis eget eleifend eget, rhoncus eu risus. Quisque laoreet congue bibendum. Donec orci mauris, efficitur eu enim ut, auctor volutpat enim. </p>
+			<?php the_field( 'middle_content_extended' ); ?>
 		
 		</div><!-- extended_content_inner -->
 		
@@ -114,65 +123,43 @@
 			
 			<div class="ex_cr_slider">
 				
-				<div class="ex_cr_slide">
-					
-					<div class="ex_cr_slide_content">
+				<?php if(get_field('case_result_slider')): ?>
+				 
+					<?php while(has_sub_field('case_result_slider')): ?>
+				 
+						<?php $case_result_image_extended = get_sub_field( 'case_result_image_extended' ); ?>
 						
-						<span class="ex_cr_amount">$2.8 million</span><!-- ex_cr_amount -->
+						<?php if ( $case_result_image_extended ) { ?>
 						
-						<span class="ex_cr_type">Brain Injury</span><!-- ex_cr_amount -->
+							<div class="ex_cr_slide">
+					
+								<img class="ex_cr_img" src="<?php echo $case_result_image_extended['url']; ?>" alt="<?php echo $case_result_image_extended['alt']; ?>"/><!-- ex_cr_img -->
+					
+							</div><!-- ex_cr_slide -->
+							
+						<?php } ?>
 						
-						<span class="ex_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull.</span><!-- ex_cr_description -->
+					<?php if(get_sub_field('case_result_amount_extended')) { ?>
+							
+							<div class="ex_cr_slide">
+					
+								<div class="ex_cr_slide_content">
 						
-					</div><!-- ex_cr_slide_content -->
-					
-				</div><!-- ex_cr_slide -->
-				
-				<div class="ex_cr_slide">
-					
-					<img class="ex_cr_img" src="<?php bloginfo('template_directory');?>/images/cr_pic.jpg"/><!-- ex_cr_img -->
-					
-				</div><!-- ex_cr_slide -->
-				
-				<div class="ex_cr_slide">
-					
-					<div class="ex_cr_slide_content">
+									<span class="ex_cr_amount"><?php the_sub_field( 'case_result_amount_extended' ); ?></span><!-- ex_cr_amount -->
 						
-						<span class="ex_cr_amount">$2.8 million</span><!-- ex_cr_amount -->
+									<span class="ex_cr_type"><?php the_sub_field( 'case_result_type_extended' ); ?></span><!-- ex_cr_amount -->
 						
-						<span class="ex_cr_type">Brain Injury</span><!-- ex_cr_amount -->
+									<span class="ex_cr_description"><?php the_sub_field( 'case_result_description_extended' ); ?></span><!-- ex_cr_description -->
 						
-						<span class="ex_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull.</span><!-- ex_cr_description -->
-						
-					</div><!-- ex_cr_slide_content -->
+								</div><!-- ex_cr_slide_content -->
 					
-				</div><!-- ex_cr_slide -->
-				
-				<div class="ex_cr_slide">
-					
-					<img class="ex_cr_img" src="<?php bloginfo('template_directory');?>/images/cr_pic.jpg"/><!-- ex_cr_img -->
-					
-				</div><!-- ex_cr_slide -->
-				
-				<div class="ex_cr_slide">
-					
-					<div class="ex_cr_slide_content">
-						
-						<span class="ex_cr_amount">$2.8 million</span><!-- ex_cr_amount -->
-						
-						<span class="ex_cr_type">Brain Injury</span><!-- ex_cr_amount -->
-						
-						<span class="ex_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull.</span><!-- ex_cr_description -->
-						
-					</div><!-- ex_cr_slide_content -->
-					
-				</div><!-- ex_cr_slide -->
-				
-				<div class="ex_cr_slide">
-					
-					<img class="ex_cr_img" src="<?php bloginfo('template_directory');?>/images/cr_pic.jpg"/><!-- ex_cr_img -->
-					
-				</div><!-- ex_cr_slide -->
+							</div><!-- ex_cr_slide -->
+							
+						<?php } ?>
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 				
 			</div><!-- ex_cr_slider -->
 			
@@ -182,15 +169,13 @@
 		
 		<div class="extended_bottom_content">
 		
-			<span class="ex_cr_title">Millions in Settlements & Verdicts</span><!-- ex_cr_title -->
+			<span class="ex_cr_title"><?php the_field( 'bottom_title_extended' ); ?></span><!-- ex_cr_title -->
 			
 			<div class="extended_bottom_content_inner">
 		
-				<h2>Talk to a Truck Accident Lawyer now</h2>
-		
-				<p>If you or a loved one has been injured, don’t wait. Contact an experienced trucking accident lawyer immediately. When we receive a call from a client who has been injured, we go to work immediately to protect their rights to full and fair compensation. Our law firm goes the extra mile, using our 40-plus years of experience to right the wrongs and make our clients whole again. If you’ve been injured, contact us now. Call Flora Templeton Stuart at (888) 782-9090 or contact us online today.</p>
+				<?php the_field( 'bottom_content_extended' ); ?>
 				
-				<a class="get_consultation_button" href="#consultation">Get Your Free Consultation</a><!-- get_consultation_button -->
+				<a class="get_consultation_button" href="#consultation"><?php the_field( 'bottom_button_extended' ); ?></a><!-- get_consultation_button -->
 			
 			</div><!-- extended_bottom_content_inner -->
 		
