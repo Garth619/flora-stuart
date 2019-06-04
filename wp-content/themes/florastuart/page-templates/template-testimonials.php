72653687c613fc2ee9ad86a806b9e6cfbf56202a
Garrett Cullen <garrettwcullen@gmail.com>
2019-06-04 15:26:06 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-testimonials.php
+++ wp-content/themes/florastuart/page-templates/template-testimonials.php
@@ -28,20 +28,178 @@ get_header(); ?>
 			
 					</div><!-- play_wrapper -->
 					
-					<div class="test_vid_overlay">
-						
-						
-						
-					</div><!-- test_vid_overlay -->
+					<div class="test_vid_overlay"></div><!-- test_vid_overlay -->
 					
 				</a>
 				
 			</div><!-- testi_vid -->
 			
+			<span class="testi_quote">“FTS has a great reputation all over Bowling Green and she’s a good lawyer.”</span><!-- testi_quote -->
+			
+			<div class="top_testi_wrapper">
+				
+				<div class="single_top_testi">
+					
+					<div class="single_top_profile">
+						
+						<img class="redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+						
+						<img class="testi_profile" src="<?php bloginfo('template_directory');?>/images/sec_five_3.png"/>
+						
+						<img class="testi_stars" src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+					</div><!-- single_top_profile -->
+					
+					<span class="single_top_quote">“Our son was in the accident with us.</span><!-- single_top_quote -->
+					
+					<span class="single_top_description">Flora took his case and was able to get him an annuity and to have it set up for later on to use it for college … We called several other lawyers and never got a response as fast as Flora did … They were there within an hour.”</span><!-- single_top_description -->
+					
+					<span class="single_top_name">Josh R.</span><!-- single_top_name -->
+					
+				</div><!-- single_top_testi -->
+				
+				<div class="single_top_testi">
+					
+					<div class="single_top_profile">
+						
+						<img class="redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+						
+						<img class="testi_profile" src="<?php bloginfo('template_directory');?>/images/sec_five_1.png"/>
+						
+						<img class="testi_stars" src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+					</div><!-- single_top_profile -->
+					
+					<span class="single_top_quote">“This particular law firm has organization skills; they have good people skills.</span><!-- single_top_quote -->
+					
+					<span class="single_top_description">They were able to anticipate the next step in the process and kept me informed the whole way through. I felt like she did such a good job, and she would do a good job for everyone who is involved in a motor vehicle collision.”</span><!-- single_top_description -->
+					
+					<span class="single_top_name">Laura P.</span><!-- single_top_name -->
+
+					
+				</div><!-- single_top_testi -->
+				
+				<div class="single_top_testi">
+					
+					<div class="single_top_profile">
+						
+						<img class="redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+						
+						<img class="testi_profile" src="<?php bloginfo('template_directory');?>/images/sec_five_2.png"/>
+						
+						<img class="testi_stars" src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+					</div><!-- single_top_profile -->
+					
+					<span class="single_top_quote">“I would recommend that you call Flora, because they make you feel important.</span><!-- single_top_quote -->
+					
+					<span class="single_top_description">I feel like I have made a friend with the staff members instead of just being a&nbsp;client.”</span><!-- single_top_description -->
+					
+					<span class="single_top_name">Charlene C.</span><!-- single_top_name -->
+
+					
+				</div><!-- single_top_testi -->
+				
+			</div><!-- top_testi_wrapper -->
+			
 		</div><!-- testimonials_inner -->
 		
 	</div><!-- testimonials_wrapper -->
+	
+	<div class="bottom_testi_wrapper">
+				
+		<div class="bottom_testi_inner">
+				
+			<div class="bottom_test_title">
+					
+				<img class="redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+					
+				<img class="star" src="<?php bloginfo('template_directory');?>/images/intl_test_icon-01.svg"/>
+				
+				<span>Hear what more clients are saying about us</span>
+					
+			</div><!-- bottom_test_title -->
+			
+			<div class="bottom_test_wrapper">
+				
+				<div class="bottom_test_col">
+					
+					<div class="single_bottom_testi">
 						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+					<div class="single_bottom_testi">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+				</div><!-- bottom_test_col -->
+				
+				<div class="bottom_test_col">
+					
+					<div class="single_bottom_testi">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+					<div class="single_bottom_testi">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+				</div><!-- bottom_test_col -->
+				
+				<div class="bottom_test_col">
+					
+					<div class="single_bottom_testi">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+					<div class="single_bottom_testi">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/stars-orange.svg"/>
+						
+						<span class="single_bottom_description">All of our needs were met in a very timely fashion. The entire process was explained in detail along with way. Thank you.</span><!-- single_bottom_description -->
+						
+						<span class="single_bottom_name">Ali T.</span><!-- single_bottom_name -->
+						
+					</div><!-- single_bottom_testi -->
+					
+				</div><!-- bottom_test_col -->
+				
+			</div><!-- bottom_test_wrapper -->
+				
+		</div><!-- bottom_testi_inner -->
+				
+	</div><!-- bottom_testi_wrapper -->
+
 </div><!-- internal_main -->
 		
 <?php get_footer(); ?>
