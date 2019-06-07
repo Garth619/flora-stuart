a6034d971c1c9e5a044988c73c564924135c92fd
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-06 17:58:42 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-biobrown.php
+++ wp-content/themes/florastuart/page-templates/template-biobrown.php
@@ -15,7 +15,13 @@ get_header(); ?>
 				
 				<div class="att_bio_sidebar">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/intl_AP_img_kent.jpg"/>
+					<?php $att_bio_image = get_field( 'att_bio_image' ); ?>
+					
+					<?php if ( $att_bio_image ) { ?>
+					
+					<img src="<?php echo $att_bio_image['url']; ?>" alt="<?php echo $att_bio_image['alt']; ?>" />
+					
+					<?php } ?>
 					
 				</div><!-- att_bio_sidebar -->
 				
@@ -23,19 +29,9 @@ get_header(); ?>
 					
 					<h1 class="att_header"><?php the_title();?></h1><!-- att_header -->
 					
-					<span class="att_postion">Attorney</span><!-- att_postion -->
+					<span class="att_postion"><?php the_field( 'att_position' ); ?></span><!-- att_postion -->
 					
-					<p>Kent R. Brown is a west Kentucky native, born and raised in Madisonville, KY. Kent’s parents are also Kentucky natives, his father a retired aircraft engine engineer for General Electric and his mother a retired high-school teacher. Kent excelled academically in high school and continued his education at the University of Louisville, where he studied political science and legal studies. During his time in college, Kent clerked for the Kentucky Commission on Human Rights and developed a strong passion for the law and for helping those in need.</p> 
-
-					<p>Kent continued his education at The John Marshall Law School in Chicago, IL, where he received his Juris Doctorate in 2010. While studying law, Kent clerked for the Hopkins County Attorney’s Office as well as Cook County State’s Attorney’s Office, where he worked within one of the largest unified court systems in the world, Cook County, Illinois. While working at the Cook County States Attorney’s Office in law school, Kent held a special license allowing him to practice law under the guidance of an Assistant State Attorney, gaining valuable court room experience. Kent also clerked in the corporate councel’s office of a Forbes Fortune 500 business while attending law school in Chicago.</p>
-					
-					<blockquote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Utas sagittis libero turpis, erdiet eros blandit nonttis libero tu”</blockquote>
-
-					<p>During his time practicing law in Chicago, Kent worked for several Fortune One-Hundred businesses and was involved in numerous high volume corporate litigations. After working in the corporate sector, Kent partnered with another attorney to open their own criminal defense practice in the heart of downtown Chicago. During their practice, Brown Zeller, LLP successfully and aggressively represented their clients, winning numerous motions and bench trials in their client’s favor.</p>
-
-					<p>In 2015 Kent accepted a position with the Kentucky Department of Public Advocacy, moving back to Kentucky to be closer to the family. It was during that Kent gained a valuable experience and developed a strong passion for serving indigent clients who needed professional guidance. While working as a public advocate Kent practiced in District, Circuit, Family and Juvenile Courts all over the Commonwealth. Litigating thousands of cases, Kent conducted several successful jury trials, argued numerous motions and conducted several appeals, all while aggressively fighting for his clients.</p>
-
-					<p>Kent has years of experience negotiating, litigating, and fighting for his clients, both inside the courtroom and out. Kent brings this same determination and fights for injured clients every day with the Law Firm of Flora Templeton Stuart.</p>
+					<?php the_field( 'att_bi_content' ); ?>
 					
 				</div><!-- att_bio_content -->
 				
@@ -44,6 +40,8 @@ get_header(); ?>
 		</div><!-- attorney_bio_wrapper -->
 		
 		</div><!-- internal_wrapper -->
+		
+		<?php if(get_field('att_bio_community_slider')): ?>
 	
 		<div class="att_bio_community">
 			
@@ -53,31 +51,19 @@ get_header(); ?>
 				
 				<div class="att_bio_slider">
 					
-					<div class="att_bio_slide">
+					<?php while(has_sub_field('att_bio_community_slider')): ?>
+					 
+							<div class="att_bio_slide">
+								
+								<?php $image = get_sub_field( 'image' ); ?>
 						
-						<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_02.jpg"/>
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 						
-					</div><!-- att_bio_slide -->
-					
-					<div class="att_bio_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_02.jpg"/>
-						
-					</div><!-- att_bio_slide -->
-					
-					<div class="att_bio_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_02.jpg"/>
-						
-					</div><!-- att_bio_slide -->
-					
-					<div class="att_bio_slide">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_02.jpg"/>
-						
-					</div><!-- att_bio_slide -->
-					
-				</div><!-- att_bio_slider -->
+							</div><!-- att_bio_slide -->
+					    
+						<?php endwhile; ?>
+					 
+					</div><!-- att_bio_slider -->
 				
 				<div class="att_bio_arrow att_bio_arrow_right"></div><!-- att_bio_arrow -->
 				
@@ -86,6 +72,8 @@ get_header(); ?>
 			<span class="comm_title">We Serve<br class="tablet"/> Our Community</span><!-- comm_title -->
 			
 		</div><!-- att_bio_community -->
+		
+		<?php endif; ?>
 	
 </div><!-- internal_main -->
 					 
