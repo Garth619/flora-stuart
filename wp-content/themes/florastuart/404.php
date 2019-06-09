98e570b755700d49d960bca93f2152f3beb73496
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-08 21:16:06 -0700

    push

--- wp-content/themes/florastuart/404.php
+++ wp-content/themes/florastuart/404.php
@@ -1,16 +1,31 @@
 <?php get_header(); ?>
 
-<h1>Not Found</h1>
-		
-		<p>The page you were looking for appears to have been moved, deleted or does not exist. You could <span class="go_back" onclick="goBack()">go back</span> to where you were or head straight to our <a href="/">home page</a>.</p>
-		
-		
-	<script type="text/javascript">
-		function goBack() {
-    	window.history.back();
-		}
-	</script>
-
+<div id="internal_trigger" class="internal_main">
 	
+<div class="internal_wrapper">
+		
+	<div class="not_found">
+			
+		<h1 class="not_found_header">Page Not Found</h1><!-- internal_header -->
+		
+		<span class="error">Error code 404</span><!-- error -->
+		
+		<span class="not_found_description">The page you are looking for was either not found or does not exist.<br/> Try refreshing the page or click the button below to go back to the Homepage.</span><!-- not_found_description -->
+		
+		<span class="not_found_button" onclick="goBack()">Go Back</span><!-- not_found_button -->
+		
+		<a class="not_found_button" href="<?php bloginfo('url');?>">Home</a><!-- not_found_button -->
+		
+	</div><!-- not_found -->
+		
+</div><!-- internal_wrapper -->
+	
+</div><!-- internal_main -->
+
+<script type="text/javascript">
+	function goBack() {
+    window.history.back();
+	}
+</script>
 
 <?php get_footer(); ?>
