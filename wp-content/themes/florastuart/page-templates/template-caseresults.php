4762ffb1c8be45011859a3ebd88b71f244148dc7
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-10 12:39:56 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-caseresults.php
+++ wp-content/themes/florastuart/page-templates/template-caseresults.php
@@ -5,7 +5,144 @@
 get_header(); ?>
 
 
-						
+<div id="internal_trigger" class="internal_main">
+		
+	<div class="internal_wrapper">
+		
+		<h1 class="internal_centered_header">Millions in Settlements</h1><!-- internal_centered_header -->
+		
+		<div class="cr_grid">
+			
+			<div class="single_cr">
+				
+				<a href="">
+				
+					<div class="inner_cr_content">
+					
+						<span class="single_cr_amount">$2.825 million</span><!-- single_cr_amount -->
+					
+						<span class="single_cr_type">Brain Injury</span><!-- single_cr_type -->
+					
+						<span class="single_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull</span><!-- single_cr_description -->
+					
+						<span class="single_cr_read_more">Read More</span><!-- single_cr_read_more -->
+					
+					</a>
+					
+				</div><!-- inner_cr_content -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/1.jpg"/>
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<a href="">
+				
+					<div class="inner_cr_content">
+					
+						<span class="single_cr_amount">$2.825 million</span><!-- single_cr_amount -->
+					
+						<span class="single_cr_type">Brain Injury</span><!-- single_cr_type -->
+					
+						<span class="single_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull</span><!-- single_cr_description -->
+					
+						<span class="single_cr_read_more">Read More</span><!-- single_cr_read_more -->
+					
+					</a>
+					
+				</div><!-- inner_cr_content -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/1.jpg"/>
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<a href="">
+				
+					<div class="inner_cr_content">
+					
+						<span class="single_cr_amount">$2.825 million</span><!-- single_cr_amount -->
+					
+						<span class="single_cr_type">Brain Injury</span><!-- single_cr_type -->
+					
+						<span class="single_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull</span><!-- single_cr_description -->
+					
+						<span class="single_cr_read_more">Read More</span><!-- single_cr_read_more -->
+					
+					</a>
+					
+				</div><!-- inner_cr_content -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/1.jpg"/>
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<a href="">
+				
+					<div class="inner_cr_content">
+					
+						<span class="single_cr_amount">$2.825 million</span><!-- single_cr_amount -->
+					
+						<span class="single_cr_type">Brain Injury</span><!-- single_cr_type -->
+					
+						<span class="single_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull</span><!-- single_cr_description -->
+					
+						<span class="single_cr_read_more">Read More</span><!-- single_cr_read_more -->
+					
+					</a>
+					
+				</div><!-- inner_cr_content -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/1.jpg"/>
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<a href="">
+				
+					<div class="inner_cr_content">
+					
+						<span class="single_cr_amount">$2.825 million</span><!-- single_cr_amount -->
+					
+						<span class="single_cr_type">Brain Injury</span><!-- single_cr_type -->
+					
+						<span class="single_cr_description">A young man with a family who suffered permanent brain damage and a crushed skull</span><!-- single_cr_description -->
+					
+						<span class="single_cr_read_more">Read More</span><!-- single_cr_read_more -->
+					
+					</a>
+					
+				</div><!-- inner_cr_content -->
+				
+			</div><!-- single_cr -->
+			
+		</div><!-- cr_grid -->
+		
+	</div><!-- internal_wrapper -->
+	
+</div><!-- internal_main -->
+					
 			  
 
 
