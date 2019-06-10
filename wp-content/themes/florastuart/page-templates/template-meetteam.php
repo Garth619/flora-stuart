6f7c765891fcdfae3bf6d81bb04cf310244e3a8d
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-09 19:30:28 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-meetteam.php
+++ wp-content/themes/florastuart/page-templates/template-meetteam.php
@@ -12,7 +12,7 @@ get_header(); ?>
 		
 		<div class="meet_attorneys_content_inner">
 		
-			<p>Established in 1976, Flora Templeton Stuart is proud to have blazed the trail for women in law as the first female attorney to try a case before a jury in Bowling Green.  With a career spanning decades, our law firm fights every day for the rights of the injured. Flora Stuart is dedicated to making a difference in the lives of her clients.  Our law firm is in the community to make a difference in the lives of others.  Each day we travel to Kentucky and Tennessee’s roads and highways to our clients who have been injured through no fault of their own to get them the justice they deserve.</p>
+			<?php the_field( 'meet_our_attorneys_content' ); ?>
 		
 		</div><!-- meet_attorneys_content_inner -->
 		
@@ -22,49 +22,121 @@ get_header(); ?>
 		
 		<div class="meet_attorneys_inner">
 			
-			<div class="single_attorney">
-				
-				<a href="">
-				
-				<div class="single_att_img">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/intl_AP_img_flora.jpg"/>
-					
-					<div class="single_att_overlay"></div><!-- single_att_overlay -->
-					
-				</div><!-- single_att_img -->
-				
-				<span class="single_att_name">Flora Templeton Stuart</span><!-- single_att_name -->
-				
-				<span class="single_att_position">Attorney and founder</span><!-- single_att_position -->
-				
-				</a>
-				
-			</div><!-- single_attorney -->
+			<?php $attorneys = get_field( 'attorneys' ); ?>
 			
-			<div class="single_attorney">
-				
-				<a href="">
-				
-				<div class="single_att_img">
+			<?php if ( $attorneys ): ?>
+			
+			<?php foreach ( $attorneys as $post ):  ?>
+			
+			<?php setup_postdata ( $post ); ?>
 					
-					<img src="<?php bloginfo('template_directory');?>/images/intl_AP_img_kent.jpg"/>
+					<div class="single_attorney">
+				
+						<a href="<?php the_permalink();?>">
+				
+							<div class="single_att_img">
 					
-					<div class="single_att_overlay"></div><!-- single_att_overlay -->
+								<?php $att_bio_image_stuart = get_field( 'att_bio_image_stuart' ); ?>
+								
+								<?php if ( $att_bio_image_stuart ) { ?>
+								
+									<img src="<?php echo $att_bio_image_stuart['url']; ?>" alt="<?php echo $att_bio_image_stuart['alt']; ?>" />
+								
+								<?php } ?>
+								
+								<?php $att_bio_image = get_field( 'att_bio_image' ); ?>
+								
+								<?php if ( $att_bio_image ) { ?>
+									
+									<img src="<?php echo $att_bio_image['url']; ?>" alt="<?php echo $att_bio_image['alt']; ?>" />
+								
+								<?php } ?>
 					
-				</div><!-- single_att_img -->
+								<div class="single_att_overlay"></div><!-- single_att_overlay -->
+					
+							</div><!-- single_att_img -->
 				
-				<span class="single_att_name">Kent Ryan Brown</span><!-- single_att_name -->
+							<div class="single_att_content">
 				
-				<span class="single_att_position">Attorney</span><!-- single_att_position -->
+								<span class="single_att_name"><?php the_title();?></span><!-- single_att_name -->
+								
+								<?php if(get_field('att_position_stuart')) { ?>
 				
-				</a>
+									<span class="single_att_position"><?php the_field( 'att_position_stuart' ); ?></span><!-- single_att_position -->
+								
+								<?php } ?>
+								
+								<?php if(get_field('att_position')) { ?>
 				
-			</div><!-- single_attorney -->
+									<span class="single_att_position"><?php the_field( 'att_position' ); ?></span><!-- single_att_position -->
+								
+								<?php } ?>
+				
+							</div><!-- single_att_content -->
+				
+						</a>
+				
+					</div><!-- single_attorney -->
+			
+			<?php endforeach; ?>
+			
+			<?php wp_reset_postdata(); ?>
+			
+			<?php endif; ?>
 			
 		</div><!-- meet_attorneys_inner -->
 		
 	</div><!-- meet_attorneys_wrapper -->
+	
+	<div class="meet_staff_wrapper">
+		
+		<div class="meet_staff_inner">
+			
+			<div class="meet_staff_title">
+				
+				<img class="redline" src="<?php bloginfo('template_directory');?>/images/test_image_icon_01.svg"/>
+				
+				<img class="law_icon" src="<?php bloginfo('template_directory');?>/images/intl_attorneys_icon-01.svg"/>
+				
+				<span class="meet_staff_header"><?php the_field( 'meet_our_staff_title' ); ?></span><!-- meet_staff_header -->
+				
+				<div class="meet_team_grid">
+					
+					<?php if(get_field('staff')): ?>
+					 
+						<?php while(has_sub_field('staff')): ?>
+					 
+							<div class="single_staffer">
+						
+								<?php $staff_profile_pic = get_sub_field( 'staff_profile_pic' ); ?>
+								
+								<?php if ( $staff_profile_pic ) : ?>
+								
+									<img src="<?php echo $staff_profile_pic['url']; ?>" alt="<?php echo $staff_profile_pic['alt']; ?>" />
+								
+								<?php else:?>
+								
+									<img src="<?php bloginfo('template_directory');?>/images/placeholder.jpg" alt="Placeholder Image"/>
+								
+								<?php endif; ?>
+						
+								<span class="staffer_name"><?php the_sub_field( 'staff_name' ); ?></span><!-- staffer_name -->
+						
+								<span class="staffer_position"><?php the_sub_field( 'staff_position' ); ?></span><!-- staffer_position -->
+						
+							</div><!-- single_staffer -->
+					    
+						<?php endwhile; ?>
+					 
+					<?php endif; ?>
+					
+				</div><!-- meet_team_grid -->
+				
+			</div><!-- meet_staff_title -->
+			
+		</div><!-- meet_staff_inner -->
+		
+	</div><!-- meet_staff_wrapper -->
 
 </div><!-- internal_main -->
 
