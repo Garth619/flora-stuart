198911833271ba6cde5d11a14bd303bd572ded31
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-09 18:13:37 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-meetteam.php
+++ wp-content/themes/florastuart/page-templates/template-meetteam.php
@@ -4,7 +4,69 @@
 
 get_header(); ?>
 
+<div id="internal_trigger" class="internal_main">
+	
+	<h1 class="internal_centered_header"><?php the_title();?></h1><!-- internal_centered_header -->
+	
+	<div class="meet_attorneys_content content">
+		
+		<div class="meet_attorneys_content_inner">
+		
+			<p>Established in 1976, Flora Templeton Stuart is proud to have blazed the trail for women in law as the first female attorney to try a case before a jury in Bowling Green.  With a career spanning decades, our law firm fights every day for the rights of the injured. Flora Stuart is dedicated to making a difference in the lives of her clients.  Our law firm is in the community to make a difference in the lives of others.  Each day we travel to Kentucky and Tennessee’s roads and highways to our clients who have been injured through no fault of their own to get them the justice they deserve.</p>
+		
+		</div><!-- meet_attorneys_content_inner -->
+		
+	</div><!-- meet_attorneys_top_content -->
+	
+	<div class="meet_attorneys_wrapper">
+		
+		<div class="meet_attorneys_inner">
+			
+			<div class="single_attorney">
+				
+				<a href="">
+				
+				<div class="single_att_img">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/intl_AP_img_flora.jpg"/>
+					
+					<div class="single_att_overlay"></div><!-- single_att_overlay -->
+					
+				</div><!-- single_att_img -->
+				
+				<span class="single_att_name">Flora Templeton Stuart</span><!-- single_att_name -->
+				
+				<span class="single_att_position">Attorney and founder</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_attorney -->
+			
+			<div class="single_attorney">
+				
+				<a href="">
+				
+				<div class="single_att_img">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/intl_AP_img_kent.jpg"/>
+					
+					<div class="single_att_overlay"></div><!-- single_att_overlay -->
+					
+				</div><!-- single_att_img -->
+				
+				<span class="single_att_name">Kent Ryan Brown</span><!-- single_att_name -->
+				
+				<span class="single_att_position">Attorney</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_attorney -->
+			
+		</div><!-- meet_attorneys_inner -->
+		
+	</div><!-- meet_attorneys_wrapper -->
 
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
