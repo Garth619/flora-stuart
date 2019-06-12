2becf570fd9e79d919bdec329533857ba668ad4d
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-12 12:22:37 -0700

    push

--- wp-content/themes/florastuart/page-templates/template-givingback.php
+++ wp-content/themes/florastuart/page-templates/template-givingback.php
@@ -13,19 +13,21 @@ get_header(); ?>
 		
 		<div class="gb_top_image">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_img.jpg"/>
+			<?php $giving_back_image = get_field( 'giving_back_image' ); ?>
+			
+			<?php if ( $giving_back_image ) { ?>
+				
+				<img src="<?php echo $giving_back_image['url']; ?>" alt="<?php echo $giving_back_image['alt']; ?>" />
+				
+			<?php } ?>
 			
 		</div><!-- gb_top_image -->
 		
 		<div class="gb_top_content content">
 			
-			<h2>Flora Templeton Stuart and our community</h2>
-
-			<p>You may see Flora and her staff along with Poppy and Pixie at community events across South Central Kentucky. Making a difference in the lives of others is the cornerstone of her practice. As a former teacher, Ms. Stuart founded Poppy Field Trip and Todays’ Leader recognizing students, teachers, and schools for service and leadership. She travels to schools all over our region often delivering school supplies.</p>
-
-			<p>BgOnStage was founded a decade ago by Ms. Stuart which is a 501 C charity bringing theater arts to schools and all ages. Ms. Stuart is proud to serve as president of the BGNoon Lions Club in 2019 to fundraise for charities. Her law firm has been a major sponsor for Toys For Tots, Stuff the Bus, Kawanis Thunderfest, Kids On The Block, International Center where she serves on the board.</p>
+			<?php the_field( 'giving_back_content' ); ?>
 			
-			<a class="poppy_button" href="#poppy-request">Invite Poppy To Your Event</a><!-- poppy_button -->
+			
 			
 		</div><!-- gb_top_content -->
 		
@@ -35,57 +37,29 @@ get_header(); ?>
 		
 		<div class="sponsorship_inner">
 		
-			<span class="sponsorship_header">Offices Held & Sponsorships</span><!-- sponsorship_header -->
+			<span class="sponsorship_header"><?php the_field( 'sponsorships_title' ); ?></span><!-- sponsorship_header -->
 		
 			<div class="sponsorship_list">
-			
-				<div class="single_sponsorship">
-					
-					<div class="single_sponsorship_inner">
 				
-						<span class="single_sponsorship_title">Bowling Green<br/> Noons Lion Club</span><!-- single_sponsorship_title -->
-				
-						<span class="single_sponsorship_subtitle">President 2019</span><!-- single_sponsorship_subtitle -->
+				<?php if(get_field('featured_sponsorships')): ?>
+				 
+					<?php while(has_sub_field('featured_sponsorships')): ?>
+				 
+						<div class="single_sponsorship">
 					
-					</div><!-- single_sponsorship_inner -->
-			
-				</div><!-- single_sponsorship -->
-			
-				<div class="single_sponsorship">
-					
-					<div class="single_sponsorship_inner">
+							<div class="single_sponsorship_inner">
 				
-						<span class="single_sponsorship_title">BG OnStage</span><!-- single_sponsorship_title -->
+								<span class="single_sponsorship_title"><?php the_sub_field( 'featured_sponsorship_title' ); ?></span><!-- single_sponsorship_title -->
 				
-						<span class="single_sponsorship_subtitle">President & Founder</span><!-- single_sponsorship_subtitle -->
+								<span class="single_sponsorship_subtitle"><?php the_sub_field( 'featured_sponsorship_subtitle' ); ?></span><!-- single_sponsorship_subtitle -->
 					
-					</div><!-- single_sponsorship_inner -->
+							</div><!-- single_sponsorship_inner -->
 			
-				</div><!-- single_sponsorship -->
-			
-				<div class="single_sponsorship">
-					
-					<div class="single_sponsorship_inner">
-				
-						<span class="single_sponsorship_title">Band Together BG</span><!-- single_sponsorship_title -->
-				
-						<span class="single_sponsorship_subtitle">Founder</span><!-- single_sponsorship_subtitle -->
-					
-					</div><!-- single_sponsorship_inner -->
-			
-				</div><!-- single_sponsorship -->
-			
-				<div class="single_sponsorship">
-					
-					<div class="single_sponsorship_inner">
-				
-						<span class="single_sponsorship_title">International Center of Kentucky</span><!-- single_sponsorship_title -->
-				
-						<span class="single_sponsorship_subtitle">Board Member</span><!-- single_sponsorship_subtitle -->
-					
-					</div><!-- single_sponsorship_inner -->
-			
-				</div><!-- single_sponsorship -->
+						</div><!-- single_sponsorship -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 			
 			</div><!-- sponsorship_list -->
 		
@@ -93,44 +67,27 @@ get_header(); ?>
 		
 		<div class="sponsorship_businesses">
 			
-			<ul>
-				<li><a href="">Poppy’s Field Trip</a></li>
-				<li><a href="">BG OnStage</a></li>
-				<li><a href="">Leader in me</a></li>
-				<li><a href="">Toys for Tots</a></li>
-				<li><a href="">Stuff the Bus</a></li>
-				<li><a href="">BG Noons Lion Club</a></li>
-				<li><a href="">International Center Of Kentucky</a></li>
-				<li><a href="">Community Education of Warren County</a></li>
-				<li><a href="">Butterflies for Maddie</a></li>
-				<li><a href="">Today’s Leader</a></li>
-				<li><a href="">Backpacks for Kidz</a></li>
-				<li><a href="">Thunderfest</a></li>
-				<li><a href="">Poppy’s Field Trip</a></li>
-				<li><a href="">BG OnStage</a></li>
-				<li><a href="">Leader in me</a></li>
-				<li><a href="">Toys for Tots</a></li>
-				<li><a href="">Stuff the Bus</a></li>
-				<li><a href="">BG Noons Lion Club</a></li>
-				<li><a href="">International Center Of Kentucky</a></li>
-				<li><a href="">Community Education of Warren County</a></li>
-				<li><a href="">Butterflies for Maddie</a></li>
-				<li><a href="">Today’s Leader</a></li>
-				<li><a href="">Backpacks for Kidz</a></li>
-				<li><a href="">Thunderfest</a></li>
-				<li><a href="">Poppy’s Field Trip</a></li>
-				<li><a href="">BG OnStage</a></li>
-				<li><a href="">Leader in me</a></li>
-				<li><a href="">Toys for Tots</a></li>
-				<li><a href="">Stuff the Bus</a></li>
-				<li><a href="">BG Noons Lion Club</a></li>
-				<li><a href="">International Center Of Kentucky</a></li>
-				<li><a href="">Community Education of Warren County</a></li>
-				<li><a href="">Butterflies for Maddie</a></li>
-				<li><a href="">Today’s Leader</a></li>
-				<li><a href="">Backpacks for Kidz</a></li>
-				<li><a href="">Thunderfest</a></li>
-			</ul>
+			<?php if(get_field('sponsorship_lists')): ?>
+			
+				<ul>
+			 
+				<?php while(has_sub_field('sponsorship_lists')): ?>
+				
+					<?php if(get_sub_field('sponsorship_website_link')) :?>
+					
+						<li><a href="<?php the_sub_field( 'sponsorship_website_link' ); ?>" target="_blank" rel="noopener"><?php the_sub_field( 'sponsorship' ); ?></a></li>
+						
+						<?php else:?>
+						
+						<li><?php the_sub_field( 'sponsorship' ); ?></li>
+					
+					<?php endif;?>
+			 
+				<?php endwhile; ?>
+				
+				</ul>
+			 
+			<?php endif; ?>
 			
 		</div><!-- sponsorship_businesses -->
 		
@@ -146,7 +103,7 @@ get_header(); ?>
 			
 				<img class="heart" src="<?php bloginfo('template_directory');?>/images/intl_givingback_icon-01.svg"/>
 			
-				<span class="gb_community_title">We Serve Our Community</span><!-- gb_community_title -->
+				<span class="gb_community_title"><?php the_field( 'we_serve_our_community_title' ); ?></span><!-- gb_community_title -->
 			
 			</div><!-- gb_community_header -->
 			
@@ -156,69 +113,32 @@ get_header(); ?>
 				
 				<div class="gb_slider">
 					
-					<div class="gb_slide">
+					<?php if(get_field('giving_back_slider')): ?>
+					 
+						<?php while(has_sub_field('giving_back_slider')): ?>
 						
-						<div class="gb_slide_inner">
-							
-							<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_01.jpg"/>
-							
-							<div class="gb_slide_overlay">
-								
-								<span class="gb_slide_caption">description of photo is the hover state description of photo is the hover state</span><!-- gb_slide_caption -->
-								
-							</div><!-- gb_slide_overlay -->
-							
-						</div><!-- gb_slide_inner -->
+							<div class="gb_slide">
 						
-					</div><!-- gb_slide -->
-					
-					<div class="gb_slide">
-						
-						<div class="gb_slide_inner">
+								<div class="gb_slide_inner">
 							
-							<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_01.jpg"/>
+									<?php $giving_back_slide = get_sub_field( 'giving_back_slide' ); ?>
+		
+									<img src="<?php echo $giving_back_slide['url']; ?>" alt="<?php echo $giving_back_slide['alt']; ?>" />
 							
-							<div class="gb_slide_overlay">
+									<div class="gb_slide_overlay">
 								
-								<span class="gb_slide_caption">description of photo is the hover state description of photo is the hover state</span><!-- gb_slide_caption -->
+										<span class="gb_slide_caption"><?php the_sub_field( 'image_caption' ); ?></span><!-- gb_slide_caption -->
 								
-							</div><!-- gb_slide_overlay -->
+									</div><!-- gb_slide_overlay -->
 							
-						</div><!-- gb_slide_inner -->
+								</div><!-- gb_slide_inner -->
 						
-					</div><!-- gb_slide -->
-					
-					<div class="gb_slide">
-						
-						<div class="gb_slide_inner">
+							</div><!-- gb_slide -->
 							
-							<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_01.jpg"/>
-							
-							<div class="gb_slide_overlay">
-								
-								<span class="gb_slide_caption">description of photo is the hover state description of photo is the hover state</span><!-- gb_slide_caption -->
-								
-							</div><!-- gb_slide_overlay -->
-							
-						</div><!-- gb_slide_inner -->
-						
-					</div><!-- gb_slide -->
-					
-					<div class="gb_slide">
-						
-						<div class="gb_slide_inner">
-							
-							<img src="<?php bloginfo('template_directory');?>/images/intl_givingback_foot_img_01.jpg"/>
-							
-							<div class="gb_slide_overlay">
-								
-								<span class="gb_slide_caption">description of photo is the hover state description of photo is the hover state</span><!-- gb_slide_caption -->
-								
-							</div><!-- gb_slide_overlay -->
-							
-						</div><!-- gb_slide_inner -->
-						
-					</div><!-- gb_slide -->
+					    
+						<?php endwhile; ?>
+					 
+					<?php endif; ?>
 					
 				</div><!-- gb_slider -->
 				
@@ -228,7 +148,7 @@ get_header(); ?>
 			
 			<div id="poppy-request" class="gb_form_wrapper">
 				
-				<span class="gb_form_title">Invite Poppy to Your Next Community Event!</span><!-- gb_form_title -->
+				<span class="gb_form_title"><?php the_field( 'invite_poppy_form_title' ); ?></span><!-- gb_form_title -->
 				
 				<div class="gb_form">
 					
