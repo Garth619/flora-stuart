27f0265a87763a62fb66a8cf92212663f570c7f4
Garrett Cullen <Garth619@users.noreply.github.com>
2019-06-17 14:07:01 -0700

    push

--- wp-content/themes/florastuart/page-templates/homepage_template_parts/section-3.php
+++ wp-content/themes/florastuart/page-templates/homepage_template_parts/section-3.php
@@ -2,7 +2,7 @@
 	
 	<div class="sec_three_inner">
 		
-		<h1>Tennessee & Kentucky Personal InjuryAttorney</h1>
+		<h1><?php the_field( 'section_three_header' ); ?></h1>
 		
 		<div class="kentucky_wrapper">
 			
@@ -11,16 +11,10 @@
 		</div><!-- kentucky_wrapper -->
 		
 		<div class="sec_three_content content">
-				
-				<h2>Personal Representation with Local Personal Injury Attorneys</h2>
-				
-				<h3>Vivamus tempor mi non turpis elementum, eu euismod ligula commodo. Vivamus lacus ligula, sodales a congue in, faucibus eu lorem. Proin lobortis consequat auctor. Nam in aliquet magna.</h3>
-				
-				<p>Nunc dapibus commodo telluseget giat. Maecenas lectus ante, bibendum ut enim in, dapibus bibendum ligula. Fusce lacus nibh, ultrices a magna ac, sollicitudin pharetra turpis. Etiam finibus sed erat vitae iaculis. In varius molestie erat. Proin ligula nisi, mattis eget eleifend eget, rhoncus eu risus. Quisque laoreet congue bibendum. Donec orci mauris, efficitur eu enim ut, auctor volutpat enim.</p>
-
-				<p>Duis finibus neque vitae lectus auctor aliquet. Aliquam vel nisl iaculis. Proin at ipsum sed nibh vehicula tempor sit amet vitae orci. Nullam at dignissim urna, lobortis hendrerit dui. Sed ligula erat, sollicitudin at elit et, posuere placerat tortor. Etiam nibh mauris, pulvinar at diam sed, pellentesque ullamcorper ex. Quisque laoreet congue bibendum. Donec orci mauris, efficitur eu enim ut, auctor volutpat enim.  Duis finibus neque vitae lectus auctor aliquet. Aliquam vel nisl iaculis. Proin at ipsum sed nibh vehicula tempor sit amet vitae orci. Nullam at dignissim urna, lobortis hendrerit dui. Sed ligula erat, sollicitudin at elit et, posuere placerat tortor. Etiam nibh mauris, pulvinar at diam sed, pellentesque ullamcorper ex.  Donec diam libero, volutpat non suscipit vitae, molestie eget ante. Vivamus metus lorem, consectetur dapibus magna in, pharetra gravida elit. Nullam efficitur, justo eget blandit finibus, ipsum metus mollis ex, at convallis urna odio vitae libero. Mauris eu risus placerat, pretium est a, imperdiet nunc. Aliquam erat volutpat. Integer vitae sem nec sapien tristique semper ut in turpis. Duis in elementum erat. Duis et leo eu felis maximus egestas. Duis accumsan ligula ultrices lacus consequat imperdiet. Mauris viverra in diam et interdum. Duis laoreet orci sed euismod tempor. Aenean sapien felis, pharetra eu metus sit amet, varius bibendum quam.</p>
-				
-			</div><!-- sec_three_content -->
+		
+			<?php the_field( 'section_three_content' ); ?>
+		
+		</div><!-- sec_three_content -->
 		
 	</div><!-- sec_three_inner -->
 	
@@ -28,35 +22,35 @@
 				
 				<div class="sec_three_image_wrapper">
 					
-					<div>
+					<?php if(get_field('section_three_image')): ?>
+					 
+						<?php while(has_sub_field('section_three_image')): ?>
+					 
+							<div>
+								
+								<?php $image = get_sub_field( 'image' ); ?>
+
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+		
+							</div>
+					    
+						<?php endwhile; ?>
+					 
+					<?php endif; ?>
 					
-						<img src="<?php bloginfo('template_directory');?>/images/mta_img_01_desk.jpg"/>
-					
-					</div>
-					
-					<div>
-					
-						<img src="<?php bloginfo('template_directory');?>/images/mta_img_02_desk.jpg"/>
-					
-					</div>
-					
-					<div>
-					
-						<img src="<?php bloginfo('template_directory');?>/images/mta_img_03_desk.jpg"/>
-					
-					</div>
-					
-					<img class="sec_three_tablet_img" src="<?php bloginfo('template_directory');?>/images/mta_img_01_tab.jpg"/>
+					<?php $section_three_tablet_image = get_field( 'section_three_tablet_image' ); ?>
+
+					<img class="sec_three_tablet_img" src="<?php echo $section_three_tablet_image['url']; ?>" alt="<?php echo $section_three_tablet_image['alt']; ?>" />
 					
 				</div><!-- sec_three_image_wrapper -->
 				
 				<div class="meet_attorneys">
 					
-					<span class="meet_attorney_title">Meet Our Attorneys</span><!-- meet_attorney_title -->
+					<span class="meet_attorney_title"><?php the_field( 'meet_attorneys_title' ); ?></span><!-- meet_attorney_title -->
 					
-					<a class="att_button" href="<?php the_permalink(789);?>">Attorney Flora Stuart</a><!-- att_button -->
+					<a class="att_button" href="<?php the_field( 'meet_flora_page_link' ); ?>">Attorney Flora Stuart</a><!-- att_button -->
 					
-					<a class="att_button" href="<?php the_permalink(60);?>">Attorney Kent Brown</a><!-- att_button -->
+					<a class="att_button" href="<?php the_field( 'meet_kent_page_link' ); ?>">Attorney Kent Brown</a><!-- att_button -->
 					
 				</div><!-- meet_attorneys -->
 				
